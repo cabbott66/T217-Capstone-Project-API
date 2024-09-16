f56693af89aad93e04ab1dbd1f4de3b3ac32c3b1
Meara1179 <xenon1179@gmail.com>
2024-09-16 20:57:32 +1000

    Begun refactoring API to be asynchronous.

---
+++ T217 Capstone Project API/Controllers/UsersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using T217_Capstone_Project_API;
+using T217_Capstone_Project_API.Models;
+using T217_Capstone_Project_API.Models.DTO;
+
+namespace T217_Capstone_Project_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly StakeholderRisksContext _context;
+
+        public UsersController(StakeholderRisksContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> PostUser(UserDTO user)
+        {
+            User newUser = new User()
+            { 
+                UserEmail = user.UserEmail,
+                UserFirstName = user.UserFirstName,
+                UserLastName = user.UserLastName
+            };
+
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = newUser.UserID }, newUser);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserID == id);
+        }
+    }
+}
--- T217 Capstone Project API/Program.cs
+++ T217 Capstone Project API/Program.cs
@@ -17,6 +17,8 @@ namespace T217_Capstone_Project_API
             {
                 options.Filters.Add(new UserAuthenticationFilterAdmin());
             });
+            builder.Services.AddDbContext<StakeholderRisksContext>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(x =>
