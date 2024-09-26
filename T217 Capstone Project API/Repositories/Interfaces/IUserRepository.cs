f6f08e9f6e91426dfe6393f2661127990ec10f04
Meara1179 <xenon1179@gmail.com>
2024-09-27 01:19:49 +1000

    User Repo commented.

--- T217 Capstone Project API/Repositories/Interfaces/IUserRepository.cs
+++ T217 Capstone Project API/Repositories/Interfaces/IUserRepository.cs
@@ -6,20 +6,61 @@ namespace T217_Capstone_Project_API.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        /// <summary>
+        /// Returns the User from the database with the matching ID.
+        /// </summary>
+        /// <param name="id">The ID of the desired User.</param>
+        /// <returns></returns>
         public abstract Task<User> GetUserAsync(int id);
 
+        /// <summary>
+        /// Returns the User from the database with the matching email.
+        /// </summary>
+        /// <param name="email">The email of the desired User.</param>
+        /// <returns></returns>
         public abstract Task<User> GetUserByEmailAsync(string email);
 
+        /// <summary>
+        /// Returns the API key of the User from the database with the matching email and password.
+        /// </summary>
+        /// <param name="email">The email of the desired User.</param>
+        /// <param name="password">The password of the desired User.</param>
+        /// <returns></returns>
         public abstract Task<string> GetApiKeyAsync(string email, string password);
 
+        /// <summary>
+        /// Returns the User from the database with the matching API key.
+        /// </summary>
+        /// <param name="apiKey">The API key of the desired User.</param>
+        /// <returns></returns>
         public abstract Task<User> GetUserByApiKeyAsync(string apiKey);
 
+        /// <summary>
+        /// Returns a list of all Users from the database.
+        /// </summary>
+        /// <returns></returns>
         public abstract Task<List<User>> GetUserListAsync();
 
+        /// <summary>
+        /// Creates a new User in the database.
+        /// </summary>
+        /// <param name="user">The UserDTO to be added to the database.</param>
+        /// <returns></returns>
         public abstract Task<User> CreateUserAsync(UserDTO user);
 
+        /// <summary>
+        /// Updates the User on the database with the specified ID with new values.
+        /// </summary>
+        /// <param name="id">The ID of the User to be updated.</param>
+        /// <param name="user">The replacement User.</param>
+        /// <returns></returns>
         public abstract Task<int> UpdateUserAsync(int id, User user);
 
+        /// <summary>
+        /// Deletes the User from the database with the matching ID.
+        /// </summary>
+        /// <param name="id">The ID of the User to be deleted.</param>
+        /// <returns></returns>
         public abstract Task<bool> DeleteUserAsync(int id);
     }
 }
