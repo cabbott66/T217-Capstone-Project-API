2f600c8966c111ee68efcb36250b024588db653a
Meara1179 <xenon1179@gmail.com>
2025-02-02 18:31:26 +1000

    StakeholderGroupRepository created (again).

---
+++ T217 Capstone Project API/Repositories/Interfaces/IStakeholderGroupRepository.cs
@@ -0,0 +1,50 @@
+﻿using T217_Capstone_Project_API.Models.DTO;
+using T217_Capstone_Project_API.Models.Projects;
+
+namespace T217_Capstone_Project_API.Repositories.Interfaces
+{
+    public interface IStakeholderGroupRepository
+    {
+        /// <summary>
+        /// Returns the StakeholderGroup from the database with the matching ID.
+        /// </summary>
+        /// <param name="id">The ID of the desired StakeholderGroup.</param>
+        /// <returns></returns>
+        public abstract Task<StakeholderGroup> GetStakeholderGroupAsync(int id);
+
+        /// <summary>
+        /// Returns a list of all StakeholderGroup from the database.
+        /// </summary>
+        /// <returns></returns>
+        public abstract Task<List<StakeholderGroup>> GetStakeholderGroupListAsync();
+
+        /// <summary>
+        /// Returns a list of all StakeholderGroup that the User has access to.
+        /// </summary>
+        /// <param name="id">The ID of the current Project.</param>
+        /// <returns></returns>
+        public abstract Task<List<StakeholderGroup>> GetStakeholderGroupListByProjectAsync(int id);
+
+        /// <summary>
+        /// Creates a new StakeholderGroup in the database.
+        /// </summary>
+        /// <param name="id">The id of the project the StakeholderGroup is associated with.</param>
+        /// <returns></returns>
+        public abstract Task<StakeholderGroup> CreateStakeholderGroupAsync(int id);
+
+        /// <summary>
+        /// Updates the StakeholderGroup with the matching ID with new values.
+        /// </summary>
+        /// <param name="id">The ID of the StakeholderGroup to be updated.</param>
+        /// <param name="project">The replacement StakeholderGroup.</param>
+        /// <returns></returns>
+        public abstract Task<int> UpdateStakeholderGroupAsync(int id, StakeholderGroup stakeholderGroup);
+
+        /// <summary>
+        /// Deletes a StakeholderGroup from the database.
+        /// </summary>
+        /// <param name="id">The ID of the StakeholderGroup to be deleted.</param>
+        /// <returns></returns>
+        public abstract Task<bool> DeleteStakeholderGroupAsync(int id);
+    }
+}
---
+++ T217 Capstone Project API/Repositories/StakeholderGroupRepository.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using T217_Capstone_Project_API.Models.Projects;
+using T217_Capstone_Project_API.Repositories.Interfaces;
+
+namespace T217_Capstone_Project_API.Repositories
+{
+    public class StakeholderGroupRepository : IStakeholderGroupRepository
+    {
+        enum UpdateStatus
+        {
+            Success,
+            BadRequest,
+            NotFound
+        }
+
+        private readonly StakeholderRisksContext _context = new StakeholderRisksContext();
+
+        public async Task<StakeholderGroup> GetStakeholderGroupAsync(int id)
+        {
+            var stakeholderGroup = await _context.StakeholderGroups.FindAsync(id);
+
+            return stakeholderGroup;
+        }
+
+        public async Task<List<StakeholderGroup>> GetStakeholderGroupListAsync()
+        {
+            var stakeholderGroupList = await _context.StakeholderGroups.OrderBy(x => x.StakeholderGroupID).ToListAsync();
+
+            return stakeholderGroupList;
+        }
+
+        public async Task<List<StakeholderGroup>> GetStakeholderGroupListByProjectAsync(int id)
+        {
+            var stakeholderGroupList = await _context.StakeholderGroups.Where(x => x.ProjectID == id).OrderBy(x => x.StakeholderGroupID).ToListAsync();
+
+            return stakeholderGroupList;
+        }
+
+        public async Task<StakeholderGroup> CreateStakeholderGroupAsync(int id)
+        {
+            StakeholderGroup newStakeholderGroup = new StakeholderGroup();
+
+            newStakeholderGroup.ProjectID = id;
+
+            _context.Add(newStakeholderGroup);
+            await _context.SaveChangesAsync();
+
+            return newStakeholderGroup;
+        }
+
+        public async Task<int> UpdateStakeholderGroupAsync(int id, StakeholderGroup stakeholderGroup)
+        {
+            if (id != stakeholderGroup.StakeholderGroupID)
+            {
+                return (int)UpdateStatus.BadRequest;
+            }
+
+            _context.StakeholderGroups.Entry(stakeholderGroup).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StakeholderGroupExists(id))
+                {
+                    return (int)UpdateStatus.NotFound;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return (int)UpdateStatus.Success;
+        }
+
+        public async Task<bool> DeleteStakeholderGroupAsync(int id)
+        {
+            var stakeholderGroup = await _context.StakeholderGroups.FindAsync(id);
+            if (stakeholderGroup == null)
+            {
+                return false;
+            }
+
+            _context.Remove(stakeholderGroup);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        // Checks the database if a StakeholderGroup with the matching ID exists.
+        private bool StakeholderGroupExists(int id)
+        {
+            return _context.StakeholderGroups.Any(e => e.StakeholderGroupID == id);
+        }
+    }
+}
