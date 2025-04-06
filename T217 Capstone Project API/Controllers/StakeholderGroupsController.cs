05d67ccfc13a8067068d8f508633fcf9dc3fad55
Meara1179 <xenon1179@gmail.com>
2025-04-06 12:19:02 +1000

    Added GetStakeholderGroupsByProjectID endpoint

--- T217 Capstone Project API/Controllers/StakeholderGroupsController.cs
+++ T217 Capstone Project API/Controllers/StakeholderGroupsController.cs
@@ -42,6 +42,23 @@ namespace T217_Capstone_Project_API.Controllers
             return stakeholderGroups;
         }
 
+        [HttpGet("GetByProjectID")]
+        [ServiceFilter(typeof(UserAuthenticationFilter))]
+        public async Task<ActionResult<IEnumerable<StakeholderGroup>>> GetStakeholderGroupsByProjectID(int projectId)
+        {
+            if (!Request.Headers.TryGetValue("x-api-key", out var apiKey)) { return BadRequest(); }
+
+            var stakeholderGroups = await _repo.GetStakeholderGroupListByProjectAsync(projectId, apiKey!);
+
+            if (!stakeholderGroups.Any())
+            {
+                return NotFound();
+            }
+
+            return stakeholderGroups;
+        }
+
+
         // GET: api/StakeholderGroups/5
         [HttpGet("{id}")]
         [ServiceFilter(typeof(UserAuthenticationFilter))]
